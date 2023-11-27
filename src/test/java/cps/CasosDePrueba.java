8be8961f0c5d74c8309912fbd126fa69e93c5048
Pobre Domingo <domingo.saavedra@tsoftglobal.com>
2023-11-27 18:36:45 -0300

    Se actualiza repo con ejemplo de preparación prueba 1 curso selenium

--- src/test/java/cps/CasosDePrueba.java
+++ src/test/java/cps/CasosDePrueba.java
@@ -1,6 +1,8 @@
-package codigo;
+package cps;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
@@ -14,47 +16,42 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+public class CasosDePrueba {
+    //Atributos
+    WebDriver driver;
+    JavascriptExecutor js;
+    String rutaProyecto = System.getProperty("user.dir"); //no es de la prueba
 
-public class WebElementsWaitSpotify {
+    String rutaDriver = rutaProyecto + "\\src\\test\\resources\\drivers\\chromedriver.exe";
 
-    //@Test
-    public void CP002_ya_se_me_ocurrira(){
-        Assertions.assertTrue(true);
-    }
-
-    //@Test
-    public void CP003_otra_cosa(){
-        Assertions.assertTrue(true);
-    }
-
-
-    //@Test
-    public void CP001_Creacion_Cta_Spotify() throws InterruptedException {
-
-        String rutaProyecto = System.getProperty("user.dir");
-
-        String rutaDriver = rutaProyecto + "\\src\\test\\resources\\drivers\\chromedriver.exe";
-
+    @BeforeEach
+    public void preCondiciones(){
         System.setProperty("webdriver.chrome.driver", rutaDriver);
+        driver = new ChromeDriver();
 
-        WebDriver driver = new ChromeDriver();
-
-        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js = (JavascriptExecutor) driver;
 
         driver.get("https://open.spotify.com/");
 
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 
+        driver.manage().window().maximize();
+    }
+
+    @AfterEach
+    public void posCondiciones(){
+        driver.quit();
+    }
+
+    @Test
+    public void CP001_creacion_cta() throws InterruptedException {
         By localizadorRegistrarte = By.xpath("//button[contains(text(),'Reg')]");
 
         WebElement btnRegistrarte = driver.findElement(localizadorRegistrarte);
 
         btnRegistrarte.click();
 
-        driver.manage().window().maximize();
-
-
-        driver.findElement(By.id("username")).sendKeys("emas1sdfsdf1dfsd23@algo.com");
+        driver.findElement(By.id("username")).sendKeys("emailTest123456@algo.com");
 
         Thread.sleep(1000);
         driver.findElement(By.xpath("//button[@data-testid='submit']")).click();
@@ -93,17 +90,6 @@ public class WebElementsWaitSpotify {
         List<WebElement> generos = wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath("//label[contains(@for,'gender')]")));
 
         generos.get(0).click();
-        Thread.sleep(2000);
-        generos.get(1).click();
-        Thread.sleep(2000);
-        generos.get(2).click();
-        Thread.sleep(2000);
-        generos.get(3).click();
-        Thread.sleep(2000);
-        generos.get(4).click();
-        Thread.sleep(2000);
-        generos.get(0).click();
-
         Thread.sleep(1000);
         driver.findElement(By.xpath("//button[@data-testid='submit']")).click();
 
@@ -113,16 +99,6 @@ public class WebElementsWaitSpotify {
 
         if(checks.size()==2){
             checks.get(0).click();
-            Thread.sleep(1000);
-            checks.get(1).click();
-            Thread.sleep(1000);
-            checks.get(0).click();
-            Thread.sleep(1000);
-            checks.get(1).click();
-            Thread.sleep(1000);
-            checks.get(0).click();
-            Thread.sleep(1000);
-            checks.get(1).click();
             Thread.sleep(1000);
         }
 
@@ -134,4 +110,42 @@ public class WebElementsWaitSpotify {
 
         Assertions.assertEquals("Curso BCI Selenium",username);
     }
+
+    @Test
+    public void CP002_iniciar_sesion() throws InterruptedException {
+
+        By localizadorIniciarSesion = By.xpath("//button[@data-testid='login-button']");
+
+        WebElement btnIniciarSesion = driver.findElement(localizadorIniciarSesion);
+
+        btnIniciarSesion.click();
+
+        driver.findElement(By.id("login-username")).sendKeys("emailTest123456@algo.com");
+
+        Thread.sleep(1000);
+
+        driver.findElement(By.id("login-password")).sendKeys("Frre3232");
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//button[@data-testid='login-button']")).click();
+        Thread.sleep(2000);
+        String username = driver.findElement(By.xpath("//button[@data-testid='user-widget-link']")).getAttribute("aria-label");
+
+        Assertions.assertEquals("Curso BCI Selenium",username);
+    }
+
+    @Test
+    public void CP003(){
+
+    }
+
+    @Test
+    public void CP004(){
+
+    }
+
+    @Test
+    public void CP005(){
+
+    }
+
 }
